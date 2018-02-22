b63acb33688c8b80522d03543fd6561d17fd3423
Goto01 <kirill.tkachev.ktkach@gmail.com>
2018-02-22 13:51:50 +0300

    Added setting of execution order of CoreUIPresentation

---
+++ Assets/CoreUI/Scripts/UICore/CoreUIInitializator.cs
@@ -0,0 +1,30 @@
+﻿using UICore.Presentation;
+using UnityEditor;
+using UnityEngine;
+
+#if UNITY_EDITOR
+
+namespace UICore
+{
+	public static class CoreUIInitializator
+	{
+		public static void SetCoreUIExecutionOrder()
+		{
+			var maxExecutionOrder = 0;
+			var targetMonoScriptName = typeof(CoreUIPresentation).Name;
+			var targetMonoScript = default(MonoScript);
+			foreach (var monoScript in MonoImporter.GetAllRuntimeMonoScripts())
+			{
+				if (monoScript.name.Equals(targetMonoScriptName))
+				{
+					targetMonoScript = monoScript;
+					continue;
+				}
+				var executionOrder = MonoImporter.GetExecutionOrder(monoScript);
+				if (executionOrder > maxExecutionOrder) maxExecutionOrder = executionOrder;
+			}
+			MonoImporter.SetExecutionOrder(targetMonoScript, Mathf.Max(maxExecutionOrder+1, MonoImporter.GetExecutionOrder(targetMonoScript)));
+		}	
+	}
+}
+#endif
