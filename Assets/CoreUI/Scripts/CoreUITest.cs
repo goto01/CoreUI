11884831be53c2e3c98975a50abe9971b85b0d16
Goto01 <kirill.tkachev.ktkach@gmail.com>
2018-02-28 16:01:44 +0300

    Added controls int the test scene

--- Assets/CoreUI/Scripts/CoreUITest.cs
+++ Assets/CoreUI/Scripts/CoreUITest.cs
@@ -21,7 +21,11 @@ internal class CoreUITest: MonoBehaviour
     private CoreUIWindow _secondWindow;
     private CoreUIFlexibleImage _barHorizontal;
     private CoreUIFlexibleImage _barVertical;
-
+    private CoreUIWindow _window0;
+    private CoreUIWindow _window1;
+    private CoreUIWindow _window0Below;
+    private CoreUIWindow _window1Below;
+    
     private string _text =
             "~The quick brown fox jumps~"
         ;
@@ -41,9 +45,9 @@ internal class CoreUITest: MonoBehaviour
         StartCoroutine(ChangeColor());
 
         _secondWindow = CoreUIEditor.Instance.Window(new Rect(_pixelSize * 155, 0, _pixelSize * 300, _pixelSize * 300), "Item Window Style");
+        _secondWindow.Enabled = false;
         CoreUIEditor.Instance.Label(new Rect(5 * _pixelSize, -5*_pixelSize, 0, 0), "±The quick brown fox jumps±", _secondWindow, 2, 1, 2, .3f, .3f, "Wave Font");
         button = CoreUIEditor.Instance.Button(new Rect(5 * _pixelSize, -20 * _pixelSize, _pixelSize * 280, 0), _secondWindow, Action);
-        _secondWindow.Active = false;
         _barHorizontal = CoreUIEditor.Instance.FlexibleImage(new Rect(5 * _pixelSize, -40 * _pixelSize, _pixelSize * 100, 0), _secondWindow, CoreUIOrientation.Horizontal ,"Health Bar Style");
         _barVertical = CoreUIEditor.Instance.FlexibleImage(new Rect(105 * _pixelSize, -40 * _pixelSize, _pixelSize * 100, 0), _secondWindow, CoreUIOrientation.Vertical ,"Health Bar Style");
         CoreUIEditor.Instance.Button(new Rect(5 * _pixelSize, -60 * _pixelSize, _pixelSize * 140, 0), _secondWindow, i =>
@@ -56,12 +60,43 @@ internal class CoreUITest: MonoBehaviour
             _barHorizontal.Value -= .1f;
             _barVertical.Value -= .1f;
         });
+
+        _window0 = CoreUIEditor.Instance.Window(new Rect(CoreUICameraHandler.Instance.LeftTopPosition, new Vector2(250 * _pixelSize, 200 * _pixelSize)), "Item Window Style");
+        CoreUIEditor.Instance.Label(new Rect(10 * _pixelSize, -10 * _pixelSize, 0, 0), "Show right window", _window0, "Wave Font");
+        CoreUIEditor.Instance.Button(new Rect(10 * _pixelSize, -25 * _pixelSize, 230 * _pixelSize, 0), _window0, ChangeVisibilityOfWindows);
+        CoreUIEditor.Instance.Label(new Rect(10 * _pixelSize, -45 * _pixelSize, 0, 0), "Activate/Disable window below", _window0, "Wave Font");
+        CoreUIEditor.Instance.Button(new Rect(10 * _pixelSize, -60 * _pixelSize, 230 * _pixelSize, 0), _window0, ChangeActivityOfWindow0Below);
+        _window0Below = CoreUIEditor.Instance.Window(new Rect(10 * _pixelSize, -80 * _pixelSize, 230 * _pixelSize, 110 * _pixelSize), _window0, "Item Window Style");
+
+        _window1 = CoreUIEditor.Instance.Window(new Rect(CoreUICameraHandler.Instance.LeftTopPosition + new Vector2(250 * _pixelSize, 0), new Vector2(250 * _pixelSize, 200 * _pixelSize)), "Item Window Style");
+        _window1.Enabled = false;
+        CoreUIEditor.Instance.Label(new Rect(10 * _pixelSize, -10 * _pixelSize, 0, 0), "Show left window", _window1, "Wave Font");
+        CoreUIEditor.Instance.Button(new Rect(10 * _pixelSize, -25 * _pixelSize, 230 * _pixelSize, 0), _window1, ChangeVisibilityOfWindows);
+        CoreUIEditor.Instance.Label(new Rect(10 * _pixelSize, -45 * _pixelSize, 0, 0), "Activate/Disable window below", _window1, "Wave Font");
+        CoreUIEditor.Instance.Button(new Rect(10 * _pixelSize, -60 * _pixelSize, 230 * _pixelSize, 0), _window1, ChangeActivityOfWindow1Below);
+        _window1Below = CoreUIEditor.Instance.Window(new Rect(10 * _pixelSize, -80 * _pixelSize, 230 * _pixelSize, 110 * _pixelSize), _window1, "Item Window Style");
+    }
+
+    private void ChangeVisibilityOfWindows(int i1)
+    {
+        _window0.Enabled = !_window0.Enabled;
+        _window1.Enabled = !_window1.Enabled;
+    }
+
+    private void ChangeActivityOfWindow0Below(int i)
+    {
+        _window0Below.Active = !_window0Below.Active;
+    }
+
+    private void ChangeActivityOfWindow1Below(int i)
+    {
+        _window1Below.Active = !_window1Below.Active;
     }
 
     private void Action(int id)
     {
-        _window.Active = !_window.Active;
-        _secondWindow.Active = !_secondWindow.Active;
+        _window.Enabled = !_window.Enabled;
+        _secondWindow.Enabled = !_secondWindow.Enabled;
     }
 
     private IEnumerator Write()
