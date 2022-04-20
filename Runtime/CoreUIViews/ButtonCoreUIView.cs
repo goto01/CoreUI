29ea442bda7033545158f7de4602a7f45fe0750b
Kiryl Tkachou <kiryl.tkachou@gmail.com>
2022-04-20 23:50:33 +0300

    Add Button view component.

---
+++ Runtime/CoreUIViews/ButtonCoreUIView.cs
@@ -0,0 +1,16 @@
+﻿namespace CoreUI
+{
+	public class ButtonCoreUIView : BaseCoreUIView
+
+	{
+		protected override CoreUIElement DrawElementInternal(CoreUIContainer parentContainer)
+		{
+			return CoreUIEditor.Instance.Button(Rect, parentContainer, Action, Style);
+		}
+
+		private void Action(int obj)
+		{
+			
+		}
+	}
+}
