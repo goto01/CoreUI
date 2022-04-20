78e797e04538019203296d75268c0ea488498477
Kiryl Tkachou <kiryl.tkachou@gmail.com>
2022-04-21 00:04:03 +0300

    Add button view clock event.

--- Runtime/CoreUIViews/ButtonCoreUIView.cs
+++ Runtime/CoreUIViews/ButtonCoreUIView.cs
@@ -1,8 +1,11 @@
-﻿namespace CoreUI
+﻿using System;
+
+namespace CoreUI
 {
 	public class ButtonCoreUIView : BaseCoreUIView
-
 	{
+		public event EventHandler Click; 
+
 		protected override CoreUIElement DrawElementInternal(CoreUIContainer parentContainer)
 		{
 			return CoreUIEditor.Instance.Button(Rect, parentContainer, Action, Style);
@@ -10,7 +13,8 @@
 
 		private void Action(int obj)
 		{
-			
+			var handler = Click;
+			if (handler != null) handler(this, EventArgs.Empty);
 		}
 	}
 }
