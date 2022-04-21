3ce2f4360d221c6de9bc2939b6175b1dba62ce4c
Kiryl Tkachou <kiryl.tkachou@gmail.com>
2022-04-22 00:39:03 +0300

    Add Label coreUI view.

---
+++ Runtime/CoreUIViews/LabelCoreUIView.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace CoreUI
+{
+	public class LabelCoreUIView : BaseCoreUIView
+	{
+		[SerializeField] private string _text;
+		
+		protected override CoreUIElement DrawElementInternal(CoreUIContainer parentContainer)
+		{
+			return CoreUIEditor.Instance.Label(Rect, _text, parentContainer, Style);
+		}
+	}
+}
